747b6281be9a5922a384b7809d8cd0401f47a7b1
Ray Ruenheck <rayruenheck@gmail.com>
2023-05-30 20:39:19 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import PokeInfos from "./pages/PokeInfos"
+
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <PokeInfos />
+    </>
+  )
+}
+
+export default App
